505821b33e9cf193192f6b049e4e8e1b18518f9d
jahona <jahona5240@gmail.com>
2020-09-13 18:10:39 +0900

    mapState Helper example

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, { mapState } from 'vuex'
 
 Vue.use(Vuex)
 
@@ -31,19 +31,51 @@ Vue.config.productionTip = false
 
 */
 const Counter = {
-  template: `<div>{{ count }}</div>`,
-  computed: {
-    count () {
-      return store.state.count
-      // return this.$store.state.count;
+  template: `
+    <div>
+      <div>
+        count: {{ count }}
+      </div>
+      <div>
+        countPlusLocalState: {{ countPlusLocalState }}
+      </div>
+    </div>
+  `,
+  data() {
+    return {
+      localCount: 10
     }
-  }
+  },
+  /*
+    mapState 를 통해 계산된 getter 함수를 생성하여 키 입력을 줄일 수 있다.
+  */
+  computed: mapState({
+    /*
+      count(state) { return state.count; } 를 의미 
+    */
+    count: state => state.count,
+
+    /*
+      this 를 사용하여 로컬 상태에 접근할려면 일반 함수를 생성해서 사용해야 한다.
+    */
+    countPlusLocalState(state) {
+      return state.count + this.localCount
+    }
+  })
+
+  /*
+  매핑된 계산된 속성의 이름이 상태 하위 트리와 같을때, 문자열 배열을 mapState 에 전달 가능
+  */
+  // computed: mapState([
+  //   'count'
+  // ])
+  
 }
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // store,
+  store,
   components: { Counter },
   template:  `
     <div class="app">
